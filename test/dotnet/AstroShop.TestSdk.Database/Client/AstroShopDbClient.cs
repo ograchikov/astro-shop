25a4b05167d3a0cf064a73067e1bb8866d4f8bf8
Oleg <6980712+ograchikov@users.noreply.github.com>
2023-06-16 01:07:44 +0600

    Added WaitAndGetProduct method.

--- test/dotnet/AstroShop.TestSdk.Database/Client/AstroShopDbClient.cs
+++ test/dotnet/AstroShop.TestSdk.Database/Client/AstroShopDbClient.cs
@@ -1,5 +1,6 @@
 ﻿namespace AstroShop.TestSdk.Database.Client;
 
+using AstroQA.TestSdk.Common.Waiter;
 using AstroQA.TestSdk.Database.Context;
 using AstroShop.TestSdk.Database.Context;
 using AstroShop.TestSdk.Domain.Model;
@@ -8,10 +9,24 @@ using Microsoft.EntityFrameworkCore;
 public class AstroShopDbClient : IAstroShopDbClient
 {
 	private readonly IDatabaseContextFactory<AstroShopDbContext> _factory;
+	private readonly IWaitHelper _waiter;
 
-	public AstroShopDbClient(IDatabaseContextFactory<AstroShopDbContext> factory)
+	public AstroShopDbClient(
+		IDatabaseContextFactory<AstroShopDbContext> factory, 
+		IWaitHelper waiter)
 	{
 		_factory = factory;
+		_waiter = waiter;
+	}
+
+	public async Task<ProductInfo?> WaitAndGetProduct(int productId)
+	{
+		await using var context = _factory.CreateContext();
+		return await _waiter
+			.WaitForEntity(async () => await context.Products
+			.AsNoTracking()
+			.FirstOrDefaultAsync(x => x.ProductId == productId)
+			.ConfigureAwait(false));
 	}
 
 	public async Task<ProductInfo?> GetProduct(int productId)
--- test/dotnet/AstroShop.TestSdk.Database/Client/IAstroShopDbClient.cs
+++ test/dotnet/AstroShop.TestSdk.Database/Client/IAstroShopDbClient.cs
@@ -4,7 +4,8 @@ using AstroShop.TestSdk.Domain.Model;
 
 public interface IAstroShopDbClient
 {
+	Task<ProductInfo?> GetProduct(int productId);
+	Task<ProductInfo?> WaitAndGetProduct(int productId);
 	Task<IReadOnlyCollection<ProductInfo>> GetProducts();
 	Task<IReadOnlyCollection<ProductInfo>> GetProducts(List<int> productsId);
-	Task<ProductInfo?> GetProduct(int productId);
 }
